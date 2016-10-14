2659f2409dc7cbfbbc49fef74bec81879b993dbd
mjb152 <martin.bower@gmail.com>
2016-10-14 14:24:39 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >  /etc/apt/sources.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >>  /etc/apt/sources.list
 apt-cache policy docker-engine
 sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get update
